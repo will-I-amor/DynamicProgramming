3ccf27b1caeac2155d08f5d0229271bfc76d9552
will-I-amor <judy_apple@hotmail.com>
2017-04-03 18:45:52 -0400

    Update easyUnderstand.py

--- maxiSubsequenceNoAdjacent/easyUnderstand.py
+++ maxiSubsequenceNoAdjacent/easyUnderstand.py
@@ -5,9 +5,10 @@ def maxSum(myarr):
     excl = 0
     for i in myarr:
         temp = incl
-        incl = max(incl,excl+i)
+        incl = max(incl,excl+i)#incl不是包括i，而是最优的i，前面incl(即不包括现在的i)和包括现在的i+前面的excl
         excl = temp
     return max(incl,excl)
 arr1 = [4,1,1,4,2,1]
 n = maxSum(arr1)
 print n
+#incl和excl不能共存么，incl现在的i，之前的i-1就不能要
